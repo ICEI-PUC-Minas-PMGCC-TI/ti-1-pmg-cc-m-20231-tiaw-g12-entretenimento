5e39424d2c4faf2383a0c03d3ae9ebf72e8fae04
Pedro-RM <98487577+Pedro-RM@users.noreply.github.com>
2023-07-01 19:03:45 -0300

    Delete Sprint 2

--- codigo/Sprint 2 - Daniel Salgado/scriptVoltar.js
+++
@@ -1,6 +0,0 @@
-/*este script foi criado, pois o botão voltar não existe na index(página principal)*/
-
-document.getElementById('btn-voltar').addEventListener('click', function (e) {
-    e.preventDefault();
-    window.location.href ="index.html";
-    });
